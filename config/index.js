235059d6f28802d76d7a7ba338c2a1f55b8f7789
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-12-07 10:09:15 +0100

    Small refactor

--- config/index.js
+++ config/index.js
@@ -1,11 +1,9 @@
 'use strict'
 
-var config = {
+module.exports = {
   username: process.env.VISMA_USERNAME || 'user',
   password: process.env.VISMA_PASSWORD || 'password',
   host: process.env.VISMA_HOST || 'hostname',
   port: process.env.VISMA_PORT || '8290',
   path: process.env.VISMA_PATH || '/hrm_ws/secure/tasks/username/'
 }
-
-module.exports = config
